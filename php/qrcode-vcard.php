4ac1fce5fb54824c3b359f04dbf1313481213ea0
Adrien Lagrange <aglsoft96@gmail.com>
2024-09-04 23:40:34 +0100

    Generate rcode with username

--- php/qrcode-vcard.php
+++ php/qrcode-vcard.php
@@ -10,10 +10,10 @@ var_dump($_POST);
 
 if(isset($_POST["name"])){
 
-	$vcard = "BEGIN:VCARD\nVERSION:3.0\nFN:".$_POST['name']."\nORG:GIS Sarl\nTEL;TYPE=WORK,MSG:+237".$_POST['phone_work']."\nTEL;TYPE=HOME,MSG:+237".$_POST['phone_home']."\nEMAIL;TYPE=INTERNET:".$_POST['email']."\nADR;TYPE=WORK:".$_POST['localization']."\nEND:VCARD";
+	$vcard = "BEGIN:VCARD\nVERSION:3.0\nFN:".$_POST['name']."\nORG:GIS Sarl\nTEL;TYPE=WORK,MSG:+237".$_POST['phone']."\nEMAIL;TYPE=INTERNET:".$_POST['email']."\nADR;TYPE=WORK:".$_POST['localization']."\nEND:VCARD";
 
 	// generation du QR Code
-	QRcode::png($vcard, $codesDir."vcard.png", QR_ECLEVEL_L, 3);
+	QRcode::png($vcard, $codesDir."vcard_".strtolower(str_replace(" ", "_", $_POST['name'])).".png", QR_ECLEVEL_L, 3);
 
 	// retour au formulaire
 	header("Location:../index.html");
